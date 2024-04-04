5dbb198ca3f6acd77b660d2e92cc7bed462b2341
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-04 10:52:34 -0700

    Estructura HTML de las páginas

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactive-dynamic-page',
+  templateUrl: './dynamic-page.component.html',
+})
+export class DynamicPageComponent {}
