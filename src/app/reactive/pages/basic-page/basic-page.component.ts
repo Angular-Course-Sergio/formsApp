21e49f33260c31ef0a9708503195631caeac0730
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-06 13:13:11 -0700

    Manejo de errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -15,7 +15,30 @@ export class BasicPageComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5090);
+    //this.myForm.reset(rtx5090);
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} characters`;
+      }
+    }
+
+    return null;
   }
 
   public myForm: FormGroup = this.fb.group({
@@ -25,7 +48,11 @@ export class BasicPageComponent implements OnInit {
   });
 
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
     this.myForm.reset({
       price: 0,
       inStorage: 0,
