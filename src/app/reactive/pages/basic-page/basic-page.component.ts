6b2e5bba57bf40699cb3d153ad53e6cd2389d44f
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-06 11:42:43 -0700

    Forms Validator

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactive-basic-page',
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  constructor(private fb: FormBuilder) {}
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  onSave(): void {
+    if (this.myForm.invalid) return;
+    console.log(this.myForm.value);
+  }
+}
