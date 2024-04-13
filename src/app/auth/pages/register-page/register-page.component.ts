c03d4ed60158d8a72081ba1f02b8b1558807147c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-13 11:09:05 -0700

    auth form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'auth-register-page',
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required, Validators.minLength(6)],
+    password2: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO: Obtener validación desde un servicio
+  }
+
+  onSave() {
+    this.myForm.markAllAsTouched();
+  }
+}
