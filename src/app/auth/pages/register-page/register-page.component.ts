a121700a635421e57eb06941872deb03eb9a9c4b
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-13 11:28:08 -0700

    validator functions

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'auth-register-page',
@@ -7,11 +8,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.required],
-    username: ['', Validators.required],
-    password: ['', Validators.required, Validators.minLength(6)],
-    password2: ['', Validators.required],
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
   });
 
   constructor(private fb: FormBuilder) {}
